d7c887b3c175adffc6323a4a311f66449ab0055e
tokhi <shafi.tokhi@gmail.com>
2015-04-28 15:59:49 +0200

    center devise signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425203242) do
+ActiveRecord::Schema.define(version: 20150428135912) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "title",            limit: 50, default: ""
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150425203242) do
     t.datetime "updated_at", null: false
     t.string   "note"
     t.string   "tags"
+    t.string   "image"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
