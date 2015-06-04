712956b0b4dc553c12bcd55a00a11b7774f979a4
tokhi <shafi.tokhi@gmail.com>
2015-06-04 17:13:58 +0200

    remove unnecessary code from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'securerandom'
-
-75.times do
-  Post.create(
-    :note => "My Post #{SecureRandom.hex(2)}",
-    :user_id => 1
-  )
-end
 
 links = {
 	"http://i452.photobucket.com/albums/qq247/damien137/gif/8z2mwn.gif"=> "Goerge bush and dancing moves",
